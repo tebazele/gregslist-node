0c3dc01fd76befd3a1e6e927d1e338fa4f9edb37
Jeanne Allen <tebazele@gmail.com>
2022-11-29 17:10:46 -0700

    jobs controller done

---
+++ server/services/JobsService.js
@@ -0,0 +1,20 @@
+class JobsService {
+    getAll(query) {
+        throw new Error("Method not implemented.");
+    }
+    getOne(id) {
+        throw new Error("Method not implemented.");
+    }
+    create(body) {
+        throw new Error("Method not implemented.");
+    }
+    remove(id) {
+        throw new Error("Method not implemented.");
+    }
+    update(id, body) {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export const jobsService = new JobsService()
